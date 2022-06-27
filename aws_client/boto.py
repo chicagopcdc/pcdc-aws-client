c56f10469d0a916795701a3e610884ced1371b04
paulmurdoch19 <80228075+paulmurdoch19@users.noreply.github.com>
2022-06-27 16:50:18 -0700

    Update boto.py

--- aws_client/boto.py
+++ aws_client/boto.py
@@ -325,7 +325,6 @@ class BotoManager(object):
                 response = self.logs_client.get_query_results(
                     queryId=queryId
                 )
-                return response
             # print(response["results"])
             # print(len(response["results"]))
     
