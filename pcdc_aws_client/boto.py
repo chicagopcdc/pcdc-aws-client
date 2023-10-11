a22f8ab0c3361ff98db6b706c4a3a60c224247c2
Paul Murdoch <pmurdoch@PED-ML-133.local>
2023-10-11 16:09:32 -0700

    update botomanager to reflect changes in boto3 regarding secretsmanager

--- pcdc_aws_client/boto.py
+++ pcdc_aws_client/boto.py
@@ -30,19 +30,21 @@ class BotoManager(object):
         self.config = config
         self.logger = logger
 
+        self.secrets_client = None
+
         if  'aws_session_token' in config or 'profile_name' in config:
              self.session = Session(**config)
              self.s3_client = self.session.client('s3')
              self.s3_resource = self.session.resource('s3')
              self.sts_client = self.session.client("sts")
              self.iam = self.session.client('iam')
-             self.secrets_client = self.session.client('secretsmanager', **config)
+             if 'region_name' in config: 
+                self.secrets_client = self.session.client('secretsmanager', **config)
         else:
             self.s3_client = client('s3', **config)
             self.s3_resource = resource('s3', **config)
             self.sts_client = client("sts", **config)
             self.iam = client('iam', **config)
-            self.secrets_client = client('secretsmanager', **config)
 
         if 'region_name' in config:
             # self.sqs_client = client("sqs", **config)
@@ -51,6 +53,8 @@ class BotoManager(object):
             self.ec2_resource = resource('ec2', **config)
             self.logs_client = client('logs', **config)
             self.batch_client = client('batch', **config)
+            if not self.secrets_client:
+                self.secrets_client = client('secretsmanager', **config)
         else:
             #self.sqs_client = None
             self.ses_client = None
